8ecc3524531d3ffb0fc8e3f62305cbcaf92558f5
Robert Master PC <robert.jonas@gmx.at>
2017-01-16 18:27:33 +0100

    mail updates: - new mail provider: a1.net -smtp without ssl

--- jrServices/jrMail.py
+++ jrServices/jrMail.py
@@ -11,15 +11,15 @@ class JrMail:
         self.myLogger = logging.getLogger('jrWetterstationLogger')
         self.myLogger.debug('Mail constructor')
 
-        self.__mailFrom = 'SeyringWetter@a1.net'
-        self.__mailTo = 'robert.jonas@gmx.at'
+        self.__mail_from = 'SeyringWetter@a1.net'
+        self.__mail_to = 'robert.jonas@gmx.at'
         secrets = netrc.netrc()
         self.__mail_user, self.__smtp_server, self.__mail_pw = secrets.authenticators('Mailprovider')
 
     # ------------------------------------------------------------------------------------------------------------------
     def sendMail(self, subject, inhalt):
-        text = 'From: ' + self.__mailFrom + '\n'
-        text += 'To: ' + self.__mailTo + '\n'
+        text = 'From: ' + self.__mail_from + '\n'
+        text += 'To: ' + self.__mail_to + '\n'
         text += 'Date: ' + time.ctime(time.time()) + '\n'
         text += 'Subject: ' + subject + '\n\n'
         text += inhalt
@@ -29,5 +29,5 @@ class JrMail:
         # server = smtplib.SMTP_SSL(self.__smtp_server)
         server = smtplib.SMTP(self.__smtp_server)
         server.login(self.__mail_user, self.__mail_pw)
-        server.sendmail(self.__mail_user, self.__mailTo, text)
+        server.sendmail(self.__mail_from, self.__mail_to, text)
         server.quit()
