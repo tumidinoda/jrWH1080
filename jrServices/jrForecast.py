c89976ba5d0751f9224f840f4cccc9f4f0a00f2f
robert@gitkraken <robert.jonas@gmx.at>
2017-02-01 17:32:23 +0100

    using jrPyCore for jrForecast

--- jrServices/jrForecast.py
+++ jrServices/jrForecast.py
@@ -40,8 +40,8 @@ params = DataStore.params(data_dir)
 Localisation.SetApplicationLanguage(params)
 hourly_data = DataStore.hourly_store(data_dir)
 idx = hourly_data.before(datetime.max)
-mailtxt = 'Zambretti(current): ' + Zambretti(params, hourly_data[idx])
-mailtxt += '\r\n\n'
+mailtxt = '- Zambretti(current): ' + Zambretti(params, hourly_data[idx])
+mailtxt += '\n- '
 
 idx = idx.replace(tzinfo=utc).astimezone(Local)
 if idx.hour < 8 or (idx.hour == 8 and idx.minute < 30):
